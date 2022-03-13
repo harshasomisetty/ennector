fa542eb46d823e34ff19f23c40bb368922941a94
Harsha Somisetty <harshasomisetty7@gmail.com>
2022-03-13 15:51:05 -0400

    basic rust program to init treasury and basic test

--- programs/ennector/src/lib.rs
+++ programs/ennector/src/lib.rs
@@ -1,14 +1,30 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("3rp6TT3ozCHM3bDw43G5zXStqqao5TwLYCTj8DgEtz8T");
 
 #[program]
 pub mod ennector {
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+
+    pub fn init_treasury(ctx: Context<InitTreasury>) -> Result<()> {
+        let treasury_account = &mut ctx.accounts.treasury_account;
+        treasury_account.data = "data".to_string();
+        // treasury_account.data = 10;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct InitTreasury<'info> {
+    #[account(init, payer = user, space = 16 + 16, seeds = [b"treasury_account".as_ref(), user.key.as_ref()],bump)]
+    // #[account(init, payer = user, space = 16 + 64)]
+    pub treasury_account: Account<'info, TreasuryAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct TreasuryAccount {
+    pub data: String,
+}
--- tests/ennector.ts
+++ tests/ennector.ts
@@ -1,16 +1,56 @@
-import * as anchor from "@project-serum/anchor";
-import { Program } from "@project-serum/anchor";
-import { Ennector } from "../target/types/ennector";
+// import * as anchor from "@project-serum/anchor";
+import {Program} from "@project-serum/anchor";
+import {Ennector} from "../target/types/ennector";
+
+const assert = require("assert");
+const anchor = require("@project-serum/anchor");
+const serumCmn = require("@project-serum/common");
+const {SystemProgram} = anchor.web3;
+import {PublicKey, Transaction, Connection, Commitment} from "@solana/web3.js";
 
 describe("ennector", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.Ennector as Program<Ennector>;
+  const connection = provider.connection;
+  const authority = anchor.web3.Keypair.generate();
+  const programID = program.idl["metadata"]["address"];
+
+  console.log("authority: ", authority.publicKey.toString());
+  console.log("provider: ", provider.wallet.publicKey.toString());
 
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.rpc.initialize({});
+
+    // let [baseAccount, accountBump] = await PublicKey.findProgramAddress(
+    //   [Buffer.from("data_account"), provider.wallet.publicKey.toBuffer()],
+    //   programID
+    // );
+
+    await provider.connection.confirmTransaction(
+      await provider.connection.requestAirdrop(authority.publicKey, 100000069),
+      "confirmed"
+    );
+
+    const tx = await program.rpc.initTreasury({
+      accounts: {
+        treasuryAccount: authority.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+    });
     console.log("Your transaction signature", tx);
+
+    const account = await program.account.treasuryAccount.fetch(
+      authority.publicKey
+    );
+
+    const accountInfo = await connection.getBalance(authority.publicKey);
+
+    console.log("data, ", account.data);
+    console.log("info", accountInfo);
+    assert.ok(account.data === "data");
   });
 });
